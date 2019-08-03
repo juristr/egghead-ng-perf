53ac05518c911a3c4df1f8ac2947ba216fa03a81
Juri <juri.strumpflohner@gmail.com>
2019-08-03 15:55:57 +0200

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
-import { EmployeeListModule } from './employee-list/employee-list.module';
-import { ListGenerator } from './tree-generator.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports: [BrowserModule, EmployeeListModule, BrowserAnimationsModule],
-  providers: [ListGenerator],
+  imports: [BrowserModule, BrowserAnimationsModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
